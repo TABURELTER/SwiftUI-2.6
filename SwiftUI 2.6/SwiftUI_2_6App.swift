6a3cb089e8f4ce7ce708658766339a88bf945e75
Дмитрий Богданов <mr.progamesyt@yandex.ru>
2024-10-05 13:16:12 +0300

    Initial Commit

---
+++ SwiftUI 2.6/SwiftUI_2_6App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_2_6App.swift
+//  SwiftUI 2.6
+//
+//  Created by Дмитрий Богданов on 05.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_2_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
