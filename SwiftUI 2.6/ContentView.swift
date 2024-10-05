2232b48dcaa1bfa1f7c4465824d69cff1692650d
Дмитрий Богданов <mr.progamesyt@yandex.ru>
2024-10-05 14:19:52 +0300

    2.6 задание

--- SwiftUI 2.6/ContentView.swift
+++ SwiftUI 2.6/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sex:Sex = .Man
+    @State var toggle:Bool = false
+    @State var news:Bool = true
+    @State var updateFromFriends:Bool = true
+    @State var alert:Bool = false
+    
+    enum Sex:String, CaseIterable, Identifiable {
+        case Man,Woman
+        var id: Self  { self }
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView{
+            Form{
+                List{
+                    Section("Личные данные"){
+                        TextField("Имя", text: .constant(""))
+                        TextField("Фамилия", text: .constant(""))
+                        DatePicker("Дата рождения", selection: .constant(Date()),displayedComponents: .date)
+                        
+                        Picker("Пол",selection: $sex){
+                            Text("Мужской").tag(Sex.Man)
+                            Text("Женский").tag(Sex.Woman)
+                        }.pickerStyle(.navigationLink)
+                    }
+                    
+                    Section("Нотификации"){
+                        Toggle(isOn: $toggle) {
+                                Text("Получать нотификации")
+                            }
+                        if toggle{
+                            Toggle(isOn: $news) {
+                                    Text("Новости")
+                                }
+                            Toggle(isOn: $updateFromFriends) {
+                                    Text("Обновления друзей")
+                                }
+                        }
+                    }
+                    
+                    Section("Документы"){
+                        Button("Личные данные"){}
+                        Button("Условия использования"){}
+                    }
+                    
+                    Section(){
+                        Button("Выход",role: .destructive){
+                            alert.toggle()
+                        }.alert(isPresented: $alert){
+                            Alert(
+                                title: Text("Выход"),
+                                message: Text("Вы уверены, что хотите выйти?"),
+                                primaryButton: .default(Text("Отменить")),
+                                secondaryButton: .destructive(Text("Выйти"))
+                            )
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
